04f04ee6470001b35d0307aef979931c702cc4a9
wangjian <1027867874@qq.com>
2019-10-10 10:22:40 +0800

    提交pytest12

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ import pytest_rerunfailures
 
 if __name__=='__main__':
     # pytest.main(['-s','-m sm','--reruns=2','--reruns-delay=5',r'--html=result\report\test.html'])
-    pytest.main (['-s', '-q', '-sm', '--reruns=2', '--reruns-delay=5', r'--alluredir=result/report/allure/'])
+    pytest.main (['-s', '-q', '-m sm', '--reruns=2', '--reruns-delay=5', r'--alluredir=result/report/allure/'])
     # pytest.main(['-s','-m sm',r'--junitxml=result\report\test.xml']) #生成xml格式的测试报告
     # pytest.main(['-m login and not demo',r'--resultlog=result\log\testlog.log']) #生成log格式的测试报告
